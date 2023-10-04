a5175964eccfae214cf08f04f37758e9feae7d05
Iamneo <techadmin@iamneo.ai>
2023-10-04 02:59:05 +0000

    Backup Commit

--- model/Laptop.java
+++ model/Laptop.java
@@ -1,1 +1,13 @@
-package com.examly.spring
+package com.examly.springapp.model;
+public class laptop{
+    private int laptopId;
+    private string laptopBrand;
+    private int laptopPrice;
+    public int getLaptopId(){
+        return laptopId;
+    }
+    public void setLaptopId(int laptopId){
+        this.laptopId = laptopId;
+        
+    }
+}
