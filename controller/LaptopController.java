1f20a607978f8b359e440ce5a55c3f35d92995d7
Iamneo <techadmin@iamneo.ai>
2023-10-04 02:23:19 +0000

    Backup Commit

---
+++ controller/LaptopController.java
@@ -0,0 +1,7 @@
+package com.springapp.controller;
+import org.springframework.web.bind.annotation.*;
+@RestController
+public class LaptopController{
+    @PostMapping("/")
+    public boolean create
+}
