cb4615c7ac73766fed7b2ac9096ccc74dd864ae1
Iamneo <techadmin@iamneo.ai>
2023-10-04 03:09:05 +0000

    Backup Commit

--- controller/LaptopController.java
+++ controller/LaptopController.java
@@ -1,9 +1,17 @@
 package com.springapp.controller;
+import com.examly.springapp.model.Laptop;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 @RestController
 public class LaptopController{
+    private List<Laptop>laptopDatabase = new ArrayList<>();
+    private int nextLaptopId = 1;
     @PostMapping("/")
     public boolean createLaptop(@RequestBody Laptop laptop){
+        laptop.setLaptopId(nextLaptopId++);
+        
         boolean success = addLaptopToDatabase(laptop);
 
         return success;
